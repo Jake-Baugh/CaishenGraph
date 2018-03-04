2b553b70f947ba3e72d51bcdf48d8ebde29d5e9f
Jake Baugh <jake@epro.com>
2018-03-04 03:30:18 +0000

    Updated UI, Pie Chart, Name Ticker

---
+++ js/piechart.js
@@ -0,0 +1,167 @@
+var svg = d3.select("#pie")
+	.append("svg")
+	.attr("id", "pie-svg")
+	.append("g")
+	
+
+svg.append("g")
+	.attr("class", "slices");
+svg.append("g")
+svg.append("g")
+	.attr("class", "labels");
+svg.append("g")
+	.attr("class", "lines");
+
+var width = 500,
+    height = 300,
+	radius = Math.min(width, height) / 2;
+
+var pie = d3.layout.pie()
+	.sort(null)
+	.value(function(d) {
+		return d.value;
+	});
+
+var arc = d3.svg.arc()
+	.outerRadius(radius * 0.8)
+	.innerRadius(radius * 0.4);
+
+var outerArc = d3.svg.arc()
+	.innerRadius(radius * 0.9)
+	.outerRadius(radius * 0.9);
+
+svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+var key = function(d){ return d.data.label; };
+
+var color = d3.scale.ordinal()
+	.domain(["Lorem ipsum", "dolor sit", "amet", "consectetur", "adipisicing", "elit", "sed", "do", "eiusmod", "tempor", "incididunt"])
+	.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+function randomData (){
+	var labels = color.domain();
+	return labels.map(function(label){
+		return { label: label, value: Math.random() }
+	});
+}
+
+
+function getData(){
+	var dict = {};
+	var labels = [];
+	
+	for (var i = 0; i < onNodes.length; i++){
+		labels.push(onNodes[i].data('id'));
+		dict[onNodes[i].data('id')] = onNodes[i].data('balance_usd') / total_usd;
+	}
+	
+	return labels.map(function(label){
+		return { label: label, value: dict[label] }
+	});
+	
+		
+}
+
+change(getData());
+
+d3.select(".randomize")
+	.on("click", function(){
+		console.log("HELLO");
+		change(getData());
+		
+	});
+	
+
+
+function change(data) {
+
+	/* ------- PIE SLICES -------*/
+	var slice = svg.select(".slices").selectAll("path.slice")
+		.data(pie(data), key);
+
+	slice.enter()
+		.insert("path")
+		.style("fill", function(d) { return color(d.data.label); })
+		.attr("class", "slice");
+
+	slice		
+		.transition().duration(animationLength) //Global transition rate?
+		.attrTween("d", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				return arc(interpolate(t));
+			};
+		})
+
+	slice.exit()
+		.remove();
+
+	/* ------- TEXT LABELS -------*/
+
+	var text = svg.select(".labels").selectAll("text")
+		.data(pie(data), key)
+		.attr("fill", "white");
+
+	text.enter()
+		.append("text")
+		.attr("dy", ".35em")
+		.attr("fill", "white")
+		.text(function(d) {
+			return d.data.label;
+		});
+	
+	function midAngle(d){
+		return d.startAngle + (d.endAngle - d.startAngle)/2;
+	}
+
+	text.transition().duration(animationLength) //global tx rate
+		.attrTween("transform", function(d) {
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * (midAngle(d2) < Math.PI ? 1 : -1);
+				return "translate("+ pos +")";
+			};
+		})
+		.styleTween("text-anchor", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				return midAngle(d2) < Math.PI ? "start":"end";
+			};
+		});
+
+	text.exit()
+		.remove();
+
+	/* ------- SLICE TO TEXT POLYLINES -------*/
+
+	var polyline = svg.select(".lines").selectAll("polyline")
+		.data(pie(data), key);
+	
+	polyline.enter()
+		.append("polyline");
+
+	polyline.transition().duration(animationLength) //global tx rate
+		.attrTween("points", function(d){
+			this._current = this._current || d;
+			var interpolate = d3.interpolate(this._current, d);
+			this._current = interpolate(0);
+			return function(t) {
+				var d2 = interpolate(t);
+				var pos = outerArc.centroid(d2);
+				pos[0] = radius * 0.95 * (midAngle(d2) < Math.PI ? 1 : -1);
+				return [arc.centroid(d2), outerArc.centroid(d2), pos];
+			};			
+		});
+	
+	polyline.exit()
+		.remove();
+};
--- js/test.js
+++ js/test.js
@@ -7,15 +7,83 @@ var data = [];
 
 data.push($.getJSON('json/cy-style.json'));
 data.push($.getJSON('json/latest.json'));
+
+var tickers = {};
+
 var total_usd = 0;
 var date;
 
-var ticker = $('.coin-page__symbol');
-var symbol = $('.coin-page__icon-img');
+var nameTicker = $('.coin-page__name');
+var symbolTicker = $('.coin-page__symbol');
+var iconTicker = $('.coin-page__icon-img');
+var totalTicker = $('#total');
 var balanceTicker = $('#balance');
 var balanceUSDTicker = $('#balanceUSD');
 var balanceBTCTicker = $('#balanceBTC');
 var dateTicker = $('#date');
+
+var onNodes = [];
+var onNode = null;
+var animationLength = 500;
+var names = {};
+
+
+
+/*
+
+function update() {
+
+    var cy = window.cy = cytoscape({
+        container: $('#cy'),
+        style: data[0],
+        elements: data[1],
+        layout: {
+            name: 'cola',
+            randomize: true,
+            edgeLength: function (edge) { return 200; },
+            infinite: true,
+            fit: false
+        },
+        ready: function () {
+    
+            cy.elements().forEach(function (ele) {
+                setSize(ele);
+                setImage(ele);
+                //setQtip(ele);
+            });
+        }
+    });
+}
+
+function render() {
+
+}
+
+function loop(timeStep) {
+    var progress = timeStep - lastStep;
+
+    update(progress);
+    render();
+
+    lastStep = timestep;
+
+    window.requestAnimationFrame(loop);
+}
+
+var lastStep = 0;
+window.requestAnimationFrame(loop);
+*/
+
+$( function() {
+
+    //$.getJSON('json/crypto.json', function (data) {
+    //    $.each(data, function(key, data) {
+    //       console.log(key[5]);
+    //        tickers.push([key, data]);
+    //    });
+    //});
+
+    //console.log(tickers);
 
 var cy = window.cy = cytoscape({
     container: $('#cy'),
@@ -26,9 +94,10 @@ var cy = window.cy = cytoscape({
         randomize: true,
         edgeLength: function (edge) { return 200; },
         infinite: true,
-        fit: false
+        fit: true
     },
     ready: function () {
+
         cy.elements().forEach(function (ele) {
             setSize(ele);
             setImage(ele);
@@ -37,57 +106,82 @@ var cy = window.cy = cytoscape({
     }
 });
 
-var onNode = null;
+var d = {};
 
-cy.on('tap', 'node', function (evt) {
-
-    var node = evt.target;
-
-    onNode = node;
-
-    ticker.text(node.id());
-    symbol.attr("src", "img/svg/" + node.id().toLowerCase() + ".svg");
-
-    updateTicker(node);
-
-});
-
-var onNodes = [];
-
-cy.on('cxttap', function (event) {
-    var eventTarget = event.target;
-    if (eventTarget === cy) {
-        //Destroy all pie nodes
-
-    } else if (eventTarget.isNode()) {
-        if (onNodes.includes(eventTarget)) {
-            onNodes.pop(eventTarget);
-            eventTarget.css({
-                'pie-1-background-opacity': 0,
-            });
-        } else {
-            onNodes.push(eventTarget);
-            updateNodes();
-        }
-
+$.ajax({
+    async: false,
+    type: "GET",
+    url: "json/crypto.json",
+	dataType: "json",
+    success: function(data)
+    {
+		names = data;
+        //for (var key in data) {
+        //    d[key] = data[key];
+        //    //console.log(d[key]);    
+        //}
+		
     }
 });
 
-setInterval(function () {
 
+cy.on('tap', 'node', function (evt) {
+    onNode = evt.target;
+
+    updateTicker(onNode);
+	
+	
+
+});
+
+cy.on('cxttap', 'node', function (event) {
+    var eventTarget = event.target;
+	updateTicker(eventTarget);
+	if (onNodes.includes(eventTarget)) {
+		var index = onNodes.indexOf(eventTarget);
+		onNodes.splice(index, 1);
+		eventTarget.css({
+			'pie-1-background-opacity': 0,
+		});
+	} else {
+		onNodes.push(eventTarget);
+		updateNodes();
+		
+	}
+});
+
+setInterval(updateData, 500);
+
+});
+
+function updateData() {
     $.getJSON('json/latest.json', function (data) {
         total_usd = data['global']['total_usd'];
         date = data['global']['date'];
+    
+    
         cy.json({ elements: data });
         cy.nodes().forEach(function (ele) {
             setSize(ele);
         });
-
+		change(getData());
         updateNodes();
         updateTicker(onNode);
+
     });
 
-}, 500);
+    $.getJSON('json/crypto.json', function (data) {
+    
+        if(onNode !== null) {
+            var id = onNode.data('id')
+        }        
+        
+        //var cache = $(nameTicker).children();
+        //nameTicker.text(data[id]).append(cache);
+    
+    });
+
+}
 
 function updateNodes() {
     for (var i = 0; i < onNodes.length; i++) {
@@ -105,7 +199,18 @@ function updateTicker(node) {
 
     balanceTicker.text(round(node.data('balance'), 8));
     balanceUSDTicker.text(round(node.data('balance_usd'), 2));
+
+    
     dateTicker.text(date.toString());
+    totalTicker.text("$" + round(total_usd, 2));
+	
+	symbolTicker.text(node.id());
+    iconTicker.attr("src", "img/black/" + node.id().toLowerCase() + ".png");
+	
+	var cache = $(nameTicker).children();
+	var id = node.data('id').toString();
+    nameTicker.text(names[id]).append(cache);
+
 
 }
 
@@ -116,8 +221,8 @@ function setSize(ele) {
             style: {
                 width: ele.data('size'), height: ele.data('size')
             },
-            duration: 400,
-            easing: 'ease-in-out'
+            duration: animationLength / 2,
+            easing: 'ease-in-out-sine'
         }
     );
 }
@@ -125,7 +230,6 @@ function setSize(ele) {
 function setImage(ele) {
 
     var icon = "img/white/" + ele.data('id'.toLowerCase()) + '.png';
-    ele.style('background-color', 'black');
     ele.style('background-image', icon);
 }
 
